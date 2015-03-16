059575876a9a55953f33ffea36ab5f9a53da8330
Robert Mikołajuk <vokiel@gmail.com>
2015-03-16 20:26:18 +0100

    Support for multiple screens
    Create click-map for multiple screens.

--- mimage.php
+++ mimage.php
@@ -1,16 +1,39 @@
 <?php
+/* Configuration - list screens with dimensions (width, height) */
+$screens = array(
+  array(1366, 768),
+  array(1680, 1050),
+  array(1920, 1080),
+);
 
-$log = file('mouse.log');
+$width = 0;
+$height = 0;
+foreach( $screens as $screen ){
+  $width += $screen[0];
+  $height = max($height, $screen[1]);
+}
 
-$im = imagecreatetruecolor(1920, 1080);
+$im = imagecreatetruecolor($width, $height);
 
 $blue = imagecolorallocatealpha($im, 0x00, 0x00, 0xff, 120);
 $red = imagecolorallocatealpha($im, 0xff, 0x00, 0x00, 120);
 
-imagefilledrectangle($im, 0, 0, 1920, 1080, 0xffffff);
+foreach( $screens as $screen ) {
+  $x = 0;
+  $y = $height-$screen[1];
 
-foreach ($log as $e) {
-    list($code, $x, $y) = explode("\t", $e);
+  if ( isset($previous_x) ){
+    $x = $previous_x;
+  }
+
+  $previous_x += $screen[0];
+
+  imagefilledrectangle($im, $x, $y, $previous_x, $height, 0xffffff);
+}
+
+$log = file('mouse.log', FILE_SKIP_EMPTY_LINES);
+foreach ( $log as $line ) {
+    list($code, $x, $y) = explode("\t", $line);
 
     $code = (int)$code;
     $x = (int)$x;
